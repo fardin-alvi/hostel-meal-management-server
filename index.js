76231281f80c99eac20e7b5d95833fe353b7f4a3
fardin-alve <fialve2809@gmail.com>
2025-01-15 20:44:48 +0600

    likes functionalitydone

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
@@ -29,12 +29,51 @@ async function run() {
         await client.connect();
 
         const userCollection = client.db('BunkInnDB').collection('users')
+        const mealCollection = client.db('BunkInnDB').collection('meals')
+        const userInteractCollection = client.db('BunkInnDB').collection('user_interaction')
 
-        app.post('/users', async(req,res)=> {
+        app.post('/users', async (req, res) => {
             const user = req.body
             const result = await userCollection.insertOne(user)
             res.send(result)
         })
+
+        app.get('/meals', async (req, res) => {
+            const result = await mealCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/meal/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await mealCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.patch('/like/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $inc: { likes: 1 }
+            };
+            const result = await mealCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        });
+
+
+        // review
+
+        app.post('/review', async (req, res) => {
+            const review = req.body
+            const result = await userInteractCollection.insertOne(review)
+            res.send(result)
+        })
+
+        app.get('/reviews', async (req, res) => {
+            const result = await userInteractCollection.find().toArray()
+            res.send(result)
+        })
+
 
 
 
