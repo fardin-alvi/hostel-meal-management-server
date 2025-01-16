a26be36c14d46877a38d2b9c9edff9ff69ef1255
fardin-alve <fialve2809@gmail.com>
2025-01-17 00:48:42 +0600

    payment api

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000
-require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors())
@@ -31,6 +33,31 @@ async function run() {
         const userCollection = client.db('BunkInnDB').collection('users')
         const mealCollection = client.db('BunkInnDB').collection('meals')
         const userInteractCollection = client.db('BunkInnDB').collection('user_interaction')
+        const packageCollection = client.db('BunkInnDB').collection('package')
+        const paymentCollection = client.db('BunkInnDB').collection('payments')
+
+        // jwt releted api
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '30d' })
+            res.send({ token })
+        })
+
+        const verifyToken = (req, res, next) => {
+            if (!req.headers.authorization) {
+                return res.status(401).send({ message: 'unAuthorized Access' })
+            }
+            const token = req.headers.authorization.split(' ')[1];
+
+            jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+                if (err) {
+                    return res.status(401).send({ message: 'unAuthorized Access' })
+                }
+                req.user = decoded
+                next()
+            })
+        }
 
         app.post('/users', async (req, res) => {
             const user = req.body
@@ -79,6 +106,17 @@ async function run() {
             res.send(result)
         });
 
+        app.get('/package', async (req, res) => {
+            const result = await packageCollection.find().toArray()
+            res.send(result)
+        } )
+        app.get('/package/:id', async (req, res) => {
+            const id = req.params.id 
+            const query = {_id: new ObjectId(id)}
+            const result = await packageCollection.findOne(query)
+            res.send(result)
+        } )
+
 
         // review
 
@@ -92,6 +130,29 @@ async function run() {
             const result = await userInteractCollection.find().toArray()
             res.send(result)
         })
+
+        // payments releted isssue
+
+        app.post('/payments', async (req, res) => {
+            const paymentInfo = req.body;
+            const result = await paymentCollection.insertOne(paymentInfo)
+            res.send(result)
+        })
+
+        app.post('/payment-intent-method', async (req, res) => {
+            const { price } = req.body
+            const payment = parseInt(price*100)
+            console.log('recived', payment)
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: payment,
+                currency: 'usd',
+                payment_method_types: ['card']
+            })
+            res.send({
+                clientSecret: paymentIntent.client_secret
+            })
+        })
+
 
 
 
